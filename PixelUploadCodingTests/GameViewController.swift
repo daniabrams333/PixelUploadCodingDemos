fc20545517117e20a89b9f55c48f3f34d3155fb8
Danielle Hampton-Abrams <daniabrams333@gmail.com>
2025-01-27 16:17:05 -0500

    updating for the new branch ... this is a test - DHA

--- PixelUploadCodingTests/GameViewController.swift
+++ PixelUploadCodingTests/GameViewController.swift
@@ -12,6 +12,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // - took out boiler plate code ... testing uploading to github - DHA
         
         let scene = GameScene(size:CGSize(width: 2048, height: 1536))
         let skView = self.view as! SKView
