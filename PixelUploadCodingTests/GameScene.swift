cde1e695fc5c559890f7fc64b03e273d54e62f86
Danielle Hampton-Abrams <daniabrams333@gmail.com>
2025-01-29 15:35:27 -0500

    changes from jan28

--- PixelUploadCodingTests/GameScene.swift
+++ PixelUploadCodingTests/GameScene.swift
@@ -13,6 +13,19 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        // Adding the background sprite here
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        
+        
+//        background.anchorPoint = CGPoint(x:0.5, y:0.5)
+//        background.position = CGPoint(x:0.5, y:0.5)
+        
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        
+        background.zRotation = CGFloat.pi / -2
+        
+        addChild(background)
         
         
     }
